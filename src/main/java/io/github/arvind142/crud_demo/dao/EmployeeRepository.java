a36cba50e2a05a5ceffc4855e58776826ca31306
Arvind Motilal Choudhary <arvindchoudhary142@yahoo.in>
2024-12-21 13:21:20 +0530

    Spring-data-rest for employee entity

--- src/main/java/io/github/arvind142/crud_demo/dao/EmployeeRepository.java
+++ src/main/java/io/github/arvind142/crud_demo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package io.github.arvind142.crud_demo.dao;
 
 import io.github.arvind142.crud_demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members ")
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
 }
